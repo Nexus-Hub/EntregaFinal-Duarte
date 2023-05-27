19258796ac3bfafec91c00634615ce3719792795
Carlos <nexusboosting@gmail.com>
2023-05-27 06:56:15 -0400

    Agregada condicion de render boton 'remove all'

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -11,7 +11,7 @@ const CartContainer = () => {
         <>
             <div className="flex gap-5 mt-4 mb-4 ml-10">
                 <h1 className="text-5xl font-bold mb-4">Cart</h1>
-                <button className="bg-red-300 rounded-full px-4 mb-4 hover:bg-red-500" onClick={clearCartItems}>Remove All Products</button>
+                {cart.length > 1 && <button className="bg-red-300 rounded-full px-4 mb-4 hover:bg-red-500" onClick={clearCartItems}>Remove All Products</button>}
             </div>
             {cart.map((product) => {
                 return (
