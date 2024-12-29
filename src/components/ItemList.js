2de8683692e0ce94cba1b089381f667c4c31bf91
Carlos Duarte - Nexus <54867638+Nexus-Hub@users.noreply.github.com>
2024-12-29 03:26:29 -0400

    Update ItemList.js
    The goold ol ? to add optional chaining

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,7 +3,7 @@ import Item from "./Item";
 const ItemList = ({ products }) => {
     return (
         <div className="grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 px-8">
-            {products.map((item) => {
+            {products?.map((item) => {
                 return (
                     <Item key={item.id} item={item} />
                 );
