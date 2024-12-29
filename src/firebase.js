873d4c6a97bd45b22f25078d1bc8e910b91bac74
Carlos Duarte - Nexus <54867638+Nexus-Hub@users.noreply.github.com>
2024-12-29 03:36:56 -0400

    Update firebase.js
    Updated const firebaseConfig.

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC2OWW6GPP_bu8mSXoBz3jIVhrSwNboYYg",
-    authDomain: "comision-39665.firebaseapp.com",
-    projectId: "comision-39665",
-    storageBucket: "comision-39665.appspot.com",
-    messagingSenderId: "661818039050",
-    appId: "1:661818039050:web:6971a471162535a7b4f8f3"
+  apiKey: "AIzaSyC2OWW6GPP_bu8mSXoBz3jIVhrSwNboYYg",
+  authDomain: "comision-39665.firebaseapp.com",
+  projectId: "comision-39665",
+  storageBucket: "comision-39665.firebasestorage.app",
+  messagingSenderId: "661818039050",
+  appId: "1:661818039050:web:6971a471162535a7b4f8f3"
 };
 
 const app = initializeApp(firebaseConfig);
